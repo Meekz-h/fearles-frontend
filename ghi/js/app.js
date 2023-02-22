6fe7afe9c26d2ef11c214a158d174e32d4dc0b8c
Mico <micohernandez27@gmail.com>
2023-02-22 15:10:53 -0500

    Card templates completed

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,4 +1,51 @@
-window.addEventListener('DOMContentLoaded', () => {
+function createCard(name, description, pictureUrl) {
+    return `
+        <div class="card shadow-lg d-grid mt-5">
+            <img src="${pictureUrl}" class="card-img-top">
+            <div class="card-body">
+            <h5 class="card-title">${name}</h5>
+            <p class="card-text text-justify">${description}</p>
+            </div>
+        </div>
+    `;
+  }
 
 
-});
+
+  window.addEventListener('DOMContentLoaded', async () => {
+
+    const url = 'http://localhost:8000/api/conferences/';
+
+    try {
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        // Figure out what to do when the response is bad
+      } else {
+          const data = await response.json();
+          let i = 0
+          for (let conference of data.conferences) {
+              i++;
+                  const detailUrl = `http://localhost:8000${conference.href}`;
+                  const detailResponse = await fetch(detailUrl);
+                  const column = document.querySelector(`#col-${i}`)
+                  if (detailResponse.ok) {
+                      const details = await detailResponse.json();
+                      const title = details.conference.name;
+                      const description = details.conference.description;
+                      const pictureUrl = details.conference.location.picture_url;
+                      const html = createCard(title, description, pictureUrl);
+                      column.innerHTML += html;
+              }
+              if (i >= 3) {
+                  i = 0;
+              }
+              }
+          }
+
+      }
+     catch (e) {
+      // Figure out what to do if an error is raised
+    }
+
+  });
