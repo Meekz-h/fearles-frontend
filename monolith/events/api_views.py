f72de04f740cc0f4439347e21441c13696ed432d
Mico <micohernandez27@gmail.com>
2023-02-23 13:02:07 -0500

    Creating a location feature added

--- monolith/events/api_views.py
+++ monolith/events/api_views.py
@@ -50,6 +50,27 @@ class ConferenceDetailEncoder(ModelEncoder):
     }
 
 
+@require_http_methods(["GET"])
+def api_list_states(request):
+    # Get the states from the database ordered by name
+    states = State.objects.all().order_by("name")
+    # Create an empty list named state_list
+    state_list = []
+    # For each state in the states from the database
+        # Create a dictionary that contains the name and
+        # abbreviation for each state
+
+        # Append the dictionary to the list
+
+    for state in states:
+        state_info = {"name":state.name,
+                      "abbreviation": state.abbreviation,
+                    }
+        state_list.append(state_info)
+    return JsonResponse({"states": state_list})
+
+
+
 @require_http_methods(["GET", "POST"])
 def api_list_conferences(request):
     """
